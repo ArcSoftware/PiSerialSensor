42c175e3b2b612bd9b9e690ffa5dc6ec3233b359
Jake T <jaketepper@gmail.com>
2017-06-09 20:21:57 -0400

    added code form RXTX.

--- src/main/java/com/theironyard/charlotte/PiSerialSensorApplication.java
+++ src/main/java/com/theironyard/charlotte/PiSerialSensorApplication.java
@@ -1,5 +1,6 @@
 package com.theironyard.charlotte;
 
+import com.theironyard.charlotte.managers.ArduinoManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,16 @@ public class PiSerialSensorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiSerialSensorApplication.class, args);
+		if (args.length != 2) {
+			System.out.println("Usage: ArduRasPi portName portSpeed");
+			System.out.println("Example: ArduRasPi /dev/ttyUSB0 9600");
+			System.exit(1);
+		}
+
+		try {
+			(new ArduinoManager()).connect(args[0], Integer.parseInt(args[1]));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/theironyard/charlotte/managers/ArduinoManager.java
+++ src/main/java/com/theironyard/charlotte/managers/ArduinoManager.java
@@ -1,8 +1,93 @@
 package com.theironyard.charlotte.managers;
 
-/**
- * Created by Jake on 6/9/17.
- */
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ArduinoManager {
 
+
+    public void connect() throws Exception {
+        String portName = "ttyUSB0";
+        int portSpeed = 9600;
+
+        System.out.println("Connecting to " + portName + " at " + portSpeed + " baud");
+
+        CommPortIdentifier portIdentifier = null;
+
+        try {
+            portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        } catch (gnu.io.NoSuchPortException e) {
+            System.out.println("Port not found, listing available ports...");
+            java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+            while (portEnum.hasMoreElements())
+                System.out.println(portEnum.nextElement().getName());
+            System.exit(1);
+        }
+
+        if (portIdentifier.isCurrentlyOwned()) {
+            System.out.println("Error: Port is currently in use");
+        } else {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
+
+            if (commPort instanceof SerialPort) {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(portSpeed, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+                        SerialPort.PARITY_NONE);
+
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+
+            } else {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }
+    }
+
+    public static class SerialReader implements Runnable {
+        InputStream in;
+
+        public SerialReader(InputStream in) {
+            this.in = in;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try {
+                while ((len = this.in.read(buffer)) > -1) {
+                    System.out.print(new String(buffer, 0, len));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class SerialWriter implements Runnable {
+        OutputStream out;
+
+        public SerialWriter(OutputStream out) {
+            this.out = out;
+        }
+
+        public void run() {
+            try {
+                int c = 0;
+                while ((c = System.in.read()) > -1) {
+                    this.out.write(c);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
