60033af31c62f4d2a676f2381523117c796dfbdc
Jake T <jaketepper@gmail.com>
2017-06-13 16:25:43 -0400

    updating jar

--- src/main/java/com/theironyard/charlotte/PiSerialSensorApplication.java
+++ src/main/java/com/theironyard/charlotte/PiSerialSensorApplication.java
@@ -3,27 +3,27 @@ package com.theironyard.charlotte;
 import com.theironyard.charlotte.managers.ArduinoManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PiSerialSensorApplication {
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public void initializeManager(ArduinoManager manager) {
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(PiSerialSensorApplication.class, args);
+		ArduinoManager manager = ctx.getBean("arduinoManager", ArduinoManager.class);
+
 		try {
 			manager.connect();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(PiSerialSensorApplication.class, args);
 	}
 
 	//to run: sudo java -jar -Djava.library.path=/usr/lib/jni PiSerialSensor-0.0.1-SNAPSHOT.jar
